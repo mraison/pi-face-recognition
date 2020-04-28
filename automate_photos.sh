1e2d834de8ff878dff611694002b0ac948afc2e8
Matthew Raison <probablymattraison@gmail.com>
2020-04-28 02:34:16 +0000

    adding the photos

--- automate_photos.sh
+++ automate_photos.sh
@@ -1,6 +1,6 @@
-for i in 0 1 2 3 4 5 6 7 8 9
+for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
 do
-	raspistill -o ./dataset/matthew_raison/photo_0${i}.jpg &
+	raspistill -o ./dataset/matthew_raison/photo_${i}.jpg &
 	BACK_PID=$!
 	wait $BACK_PID
 done
